78179634fb92b392a21d46d32a92f8cc84ac2f9e
thomasrosen <thomas.rosen@me.com>
2021-07-08 08:01:23 +0200

    imported css from volt.link
    (buttons, typo, inputs, sizes)
    TODO: Some stuff is parallel to what Marvin started. This needs to be fixed.

---
+++ src/parts/button_row/button_row.stories.js
@@ -0,0 +1,9 @@
+import { wrapDefault, wrapPurple } from '../../utils/modes.js'
+import button_row from './button_row.html?raw'
+
+export default {
+  title: 'Elements/Button Row'
+}
+
+export const WhiteTheme = () => wrapDefault(button_row)
+export const PurpleTheme = () => wrapPurple(button_row)
